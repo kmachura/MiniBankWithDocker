76d6499a8d387c8c0224479d489e0937497f03d0
Alexshailiha <Alexshailiha@gmail.com>
2023-01-27 19:32:30 +0100

    pages login

--- frontend/mini-bank/src/app/app-routing.module.ts
+++ frontend/mini-bank/src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { KontoComponent } from './konto/konto.component';
+import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
 import { PobranieComponent } from './pobranie/pobranie.component';
 import { WalutyComponent } from './waluty/waluty.component';
+
+
 
 const routes: Routes = [
   {path:"waluty",component:WalutyComponent},
   {path:"",component:MainComponent},
   {path:"konto",component:KontoComponent},
-  {path:"pobranie",component:PobranieComponent}
+  {path:"pobranie",component:PobranieComponent},
+  {path:"login",component:LoginComponent},
+  
+  
+
 
 ];
 
